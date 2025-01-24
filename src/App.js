2ef9414c69652508bd0aac3945817a0fbb11e904
Aracely0504 <aracelybc90@gmail.com>
2025-01-23 22:31:57 -0800

    Added project from codesand box

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import WeatherSearch from "./WeatherSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <WeatherSearch />
+      </header>
+    </div>
+  );
+}
+
+export default App;
