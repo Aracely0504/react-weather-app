988fd19cbd7838d196be8abb37846593c0d10ad6
Aracely0504 <aracelybc90@gmail.com>
2025-01-23 22:46:29 -0800

    updated Babel plugin

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <WeatherSearch />
+        <p>
+          {" "}
+          This project was coded by{" "}
+          <a href="https://github.com/Aracely0504">Aracely Campos </a> and is{" "}
+          <a href="https://github.com/Aracely0504/react-weather-app">
+            open-sourced on Github
+          </a>{" "}
+          and
+          <a href="http://www.example.com"> hosted on Netlify.</a>
+        </p>
       </header>
     </div>
   );
